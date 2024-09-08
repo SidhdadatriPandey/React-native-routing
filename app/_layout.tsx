edb5a2a5bc519e7b586b7fefdf832071d76a5b23
SidhdadatriPandey <sidhdadatripandey@gmail.com>
2024-09-08 10:39:28 +0530

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,10 +26,9 @@ export default function RootLayout() {
   }
   return (
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack initialRouteName='index' screenOptions={{ headerShown: false }}>
-          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
-          <Stack.Screen name='index' />
         </Stack>
       </ThemeProvider>
 
